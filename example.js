10de50449f564c27302b07ab03add9d5d86ca44e
Hinell <al.neodim@gmail.com>
2015-06-02 03:26:02 +0300

    Deprecated .sendAsStreams().
    	Visual fixes.
    	Update example.
    	Renamed main class

--- example.js
+++ example.js
@@ -1,10 +1,11 @@
 var WriteStream     = require("fs").WriteStream;
-var FilesStream     = require("."); // our module
+var FilesStream     = require(".");
     stream          = new FilesStream("./ofMyFiles.js");
     stream.addFiles([
-                        "./example.js",
-                        "./readme.md",
-                        "./errorFile"
+                        './test/file.1.txt',
+                        './test/file.2.txt',
+                        './test/file.3.txt',
+                        './test/file.not.exists.txt'
                     ])
     stream.on("error", function (err) {
         if (err) console.log(" Ooops! File /errorFile don't exists:\r\n",err.message);
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Readable     = require('stream').Readable
 var iherits      = require("util").inherits
 var fs           = require("fs")
 var ReadStream   = fs.ReadStream
-    function  Bundle (name,options) {
+    function  FilesStream (name,options) {
         if (typeof name === "string") this.name = name
         else                          options   = name
         Readable.call(this,options)
@@ -15,23 +15,22 @@ var ReadStream   = fs.ReadStream
         this.__fExists      = true
     };
 
-    iherits(Bundle,Readable)
-    module.exports = Bundle
-    Bundle.prototype.addFiles           = function (array) {
+    iherits(FilesStream,Readable)
+    module.exports = FilesStream
+    FilesStream.prototype.addFiles           = function (array) {
         var notArray  = !(array instanceof Array)
         if (notArray) throw new Error ("Required an array containing paths")
         this._sources = array
     }
-    Bundle.prototype._read              = function (chunkSize) {
+    FilesStream.prototype._read              = function (chunkSize) {
         var filePath  = this._sources[this._index]
         if (this._isLastSource())   { this.push(null); return }
-         chunkSize && this._sendAsChunks(filePath,chunkSize)
-        !chunkSize && this._sendAsStream(filePath)
-
+        this._sendChunksOf(filePath,chunkSize)
     }
-    Bundle.prototype._isLastSource      = function (        ) { return this._index === this._sources.length }
-    Bundle.prototype._sendAsChunks      = function (filePath,chunkSize) {
-        function failFail (err) {
+    FilesStream.prototype._isLastSource      = function (        ) { return this._index === this._sources.length }
+    FilesStream.prototype._sendChunksOf      = function (filePath,chunkSize) {
+        function readFail (err,fd) {
+            fd && fs.close(fd)
             this.emit("error",err)
             this._index++
         }
@@ -40,16 +39,16 @@ var ReadStream   = fs.ReadStream
         var read    = fs.read
         var close   = fs.close
         open(filePath,"r", (function (err, fd) {
-            if (err) {failFail.call(this,err);return}
+            if (err) { readFail.call(this,err,fd);return}
             stat(filePath, (function (err, stats) {
-                if (err) {failFail.call(this,err);return}
+                if (err) { readFail.call(this,err,fd);return}
                 var sizeofFile  = stats.size
-                var read        = this.__fRead
-                if (read !== sizeofFile){
-                    if ((read+chunkSize)>sizeofFile) chunkSize = sizeofFile-read
+                var readed      = this.__fRead
+                if (readed !== sizeofFile){
+                    if ((readed+chunkSize)>sizeofFile) chunkSize = sizeofFile-readed
                     var buffer = new Buffer(chunkSize,this.encoding)
-                    fs.read(fd,buffer,0,chunkSize,read, (function (err,l,buff) {
-                        if (err) {failFail.call(this,err);return}
+                    read(fd,buffer,0,chunkSize,readed, (function (err,l,buff) {
+                        if (err) { readFail.call(this,err,fd);return}
                         this.push(buff)
                         this.__fRead+=chunkSize
                     }).bind(this))
@@ -58,28 +57,8 @@ var ReadStream   = fs.ReadStream
                     this._index++
                     this.push(new Buffer(this._dataDelimiter || "\r\n",this.encoding))
                     close(fd)
-                    //console.log(" %s | %s | %s: %s | Chunk: %s | Read: %s:%s |   %s:%s ",this._index,this._dbg,filePath,sizeofFile,chunkSize,read,typeof read,sizeofFile, typeof sizeofFile)
+                    this.emit('close')
                 }
-
             }).bind(this))
         }).bind(this))
-    }
-    Bundle.prototype._sendAsStream      = function (filePath) {
-
-        this.currentStream  = this.currentStream || new ReadStream(filePath,this.options)
-        this.currentStream.once("data", (function (chunk) {
-            if (typeof chunk === "string") chunk+=("\r\n")
-            this.push(new Buffer(this._dataDelimiter || "\r\n",this.encoding))
-            this._index++
-            this.currentStream = false
-        }).bind(this))
-        this.currentStream._errExtracted || (function () {
-            var lstrns = this.listeners("error")
-            if(lstrns instanceof Array){
-                this.currentStream._errExtracted = !0;
-                for (var i in lstrns) this.currentStream.on("error",lstrns[i])
-
-            }
-        }).call(this)
-
     }
